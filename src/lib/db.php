f4db0321baab558a15b70354778f89976e1c893e
Nicolò Luigi Allegris <77175052+MrTip0@users.noreply.github.com>
2023-01-02 17:12:28 +0100

    Update db.php

--- src/lib/db.php
+++ src/lib/db.php
@@ -2,7 +2,7 @@
 function getDB(): mysqli {
     $db = mysqli_connect("127.0.0.1", "utente", "password", "persone");
     if (!$db) {
-        die("impossibile connttersi al database");
+        die("impossibile connettersi al database");
     }
     return $db;
 }
