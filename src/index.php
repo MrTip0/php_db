912e6444fbc08da5b605169150bf77163241e346
Nicolò Luigi Allegris <nico@allgraf.it>
2023-01-12 18:27:32 +0100

    implemented pagination on index page

--- src/index.php
+++ src/index.php
@@ -9,6 +9,7 @@
     </head>
     <body class="m-4">
         <form action="" method="get">
+            <input type="hidden" name="from" value="0">
             <div class="mb-3 row">
                 <label for="inputId" class="col-sm-2 col-form-label">Id</label>
                 <div class="col-sm-10">
@@ -31,7 +32,7 @@
                 <button type="submit" class="btn btn-primary">Invia</button>
             </div>
         </form>
-        <table class="table table-striped mt-5">
+        <table class="table table-striped mt-5" id="data-table">
             <thead>
                 <tr>
                     <th scope="col">#</th>
@@ -47,17 +48,21 @@
         $db = getDB();
 
         $fields = 'id, nome, cognome, DATE_FORMAT(data_nascita, \'%d/%m/%Y\') AS data_nascita';
-        $q = "SELECT $fields FROM dati_txt WHERE id = ?";
+        $q = "SELECT $fields FROM dati_txt WHERE id = ? LIMIT ?, 25";
+        $nq = "SELECT COUNT(id) FROM dati_txt WHERE id = ?";
         $data = "0";
 
         if (!empty($_GET["id"])) {
-            $q = "SELECT $fields FROM dati_txt WHERE id = ?";
+            $q = "SELECT $fields FROM dati_txt WHERE id = ? ORDER BY id LIMIT ?, 25";
+            $nq = "SELECT COUNT(id) FROM dati_txt WHERE id = ?";
             $data = $_GET["id"];
         } elseif(!empty($_GET["cognome"])) {
-            $q = "SELECT $fields FROM dati_txt WHERE cognome LIKE ?";
+            $q = "SELECT $fields FROM dati_txt WHERE cognome LIKE ? ORDER BY id LIMIT ?, 25";
+            $nq = "SELECT COUNT(id) FROM dati_txt WHERE cognome LIKE ?";
             $data = '%'.$_GET["cognome"].'%';
         } elseif(!empty($_GET["nome"])) {
-            $q = "SELECT $fields FROM dati_txt WHERE nome LIKE ?";
+            $q = "SELECT $fields FROM dati_txt WHERE nome LIKE ? ORDER BY id LIMIT ?, 25";
+            $nq = "SELECT COUNT(id) FROM dati_txt WHERE nome LIKE ?";
             $data = '%'.$_GET["nome"].'%';
         }
 
@@ -65,15 +70,31 @@
         if (!$q) {
             die($db->error);
         }
-
-        $q->bind_param("s", $data);
-
+        
+        $from = intval($_GET["from"] ?? 0);
+        $q->bind_param("si", $data, $from);
+        
         if (!$q->execute()) {
             die($q->error);
         }
-
-
+        
+        
         $r = $q->get_result();
+
+
+        $num = $db->prepare($nq);
+        if (!$nq) {
+            die($db->error);
+        }
+        
+        $num->bind_param("s", $data);
+        
+        if (!$num->execute()) {
+            die($num->error);
+        }
+
+        $rn = $num->get_result()->fetch_all(MYSQLI_NUM)[0][0];
+        
         while ($arr = $r->fetch_assoc()) {
             ?>
             <tr>
@@ -98,6 +119,23 @@
         ?>
             </tbody>
         </table>
+        <div class="row text-center mb-5 mt-3">
+            <a class="col btn btn-primary text-center <?php if (floor($from / 25 + 1) <= 1) { echo "disabled"; } ?>"
+                        href="?from=<?php echo $from - 25?>&id=<?php echo $_GET["id"]??"" ?>&nome=<?php echo $_GET["nome"]??"" ?>&cognome=<?php echo $_GET["cognome"]??"" ?>#data-table">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                </svg>
+            </a>
+            <div class="col align-content-center row">
+                <h4 class="mb-0"><?php echo floor($from / 25 + 1) ?>/<?php echo max(1, floor($rn/25)) ?></h4>
+            </div>
+            <a class="col btn btn-primary <?php if (floor($from / 25 + 1) >= max(1, floor($rn/25))) { echo "disabled"; } ?>"
+                href="?from=<?php echo $from + 25?>&id=<?php echo $_GET["id"]??"" ?>&nome=<?php echo $_GET["nome"]??"" ?>&cognome=<?php echo $_GET["cognome"]??"" ?>#data-table">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+                </svg>
+            </a>
+        </div>
         <div class="row fixed-bottom">
             <a href="add/" class="btn btn-primary col">Aggiungi persona</a>
             <a href="statistiche/" class="btn btn-primary col">Statistiche</a>
